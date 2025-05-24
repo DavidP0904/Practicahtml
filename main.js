b81c4e011444177d8ddc2313be3f3c4e9ff739ee
unknown <vidpebe@gmail.com>
2025-05-23 20:26:03 -0400

    practica2

--- main.js
+++ main.js
@@ -25,5 +25,33 @@ function decrementar() {
 
 function reiniciar() {
     //reiniciar el valor del contador
-    document.getElementById("contador").textContent = 0;
+    document.getElementById("cont").textContent = 0;
 }
+function incrementar2() {
+    //obtener el valor del contador
+    let valor = document.getElementById("cont").textContent;
+    //convertir el valor a un entero
+    valor = parseInt(valor);
+    //incrementar el valor
+    valor++;
+    //actualizar el valor del contador
+    document.getElementById("cont").textContent = valor;
+}
+function decrementar2() {
+    //obtener el valor del contador
+    let valor = document.getElementById("cont").textContent;
+    //convertir el valor a un entero
+    valor = parseInt(valor);
+    //decrementar el valor
+    valor--;
+    //actualizar el valor del contador
+    document.getElementById("cont").textContent = valor;
+    if (valor < 0) {
+        //si el valor es menor que 0, reiniciar el contador
+        reiniciar2();
+    }
+}
+function reiniciar2() {
+    //reiniciar el valor del contador
+    document.getElementById("cont").textContent = 0;
+}   
